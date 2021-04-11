514f0cf24ff5954d18f8bf201c9bdf142d9e45ab
Henry Augusta <henryaugusta>
2021-04-11 22:38:25 +0700

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -49,12 +49,12 @@
             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-chart-bar"></i> <span>Laporan/Report</span></a>
             <ul class="dropdown-menu">
               <li class="{{ set_active('arsip.ct') }}"><a class="nav-link" href="{{ route('arsip.ct')}}">Daftar Arsip</a></li>
-              <li class="{{ set_active('op-berkas.ct') }}"><a class="nav-link" href="{{ route('op-berkas.ct')}}">Laporan Stock Opname Berkas</a></li>
-              <li class="{{ set_active('op-buku.ct') }}"><a class="nav-link" href="{{ route('op-buku.ct')}}">Laporan Stock Opname Buku</a></li>
-              <li class="{{ set_active('peminjaman-berkas.ct') }}"><a class="nav-link" href="{{ route('peminjaman-berkas.ct')}}">Laporan Peminjaman Berkas</a></li>
-              <li class="{{ set_active('peminjaman-buku.ct') }}"><a class="nav-link" href="{{ route('peminjaman-buku.ct')}}">Laporan Peminjaman Buku</a></li>
-              <li class="{{ set_active('pengembalian-berkas.ct') }}"><a class="nav-link" href="{{ route('pengembalian-berkas.ct')}}">Laporan Pengembalian Berkas</a></li>
-              <li class="{{ set_active('pengembalian-buku.ct') }}"><a class="nav-link" href="{{ route('pengembalian-buku.ct')}}">Laporan Pengembalian Buku</a></li>
+              <li class="{{ set_active('op-berkas.ct') }}"><a class="nav-link" href="{{ route('op-berkas.ct')}}"> <p>Laporan Stock Opname Berkas</p></a> </li>
+              <li class="{{ set_active('op-buku.ct') }}"><a class="nav-link" href="{{ route('op-buku.ct')}}"> <p> Laporan Stock Opname Buku</p></a></li>
+              <li class="{{ set_active('peminjaman-berkas.ct') }}"><a class="nav-link" href="{{ route('peminjaman-berkas.ct')}}"> <p>Laporan Peminjaman Berkas</p> </a></li>
+              <li class="{{ set_active('peminjaman-buku.ct') }}"><a class="nav-link" href="{{ route('peminjaman-buku.ct')}}"> <p>Laporan Peminjaman Buku</p> </a></li>
+              <li class="{{ set_active('pengembalian-berkas.ct') }}"><a class="nav-link" href="{{ route('pengembalian-berkas.ct')}}"> <p>Laporan Pengembalian Berkas</p> </a></li>
+              <li class="{{ set_active('pengembalian-buku.ct') }}"><a class="nav-link" href="{{ route('pengembalian-buku.ct')}}"> <p>Laporan Pengembalian Buku</p> </a></li>
               <hr>
 {{-- 
               <li class="{{ set_active('op-berkas.cf') }}"><a class="nav-link" href="{{ route('op-berkas.cf')}}">Cetak Opname Berkas Pertanggal</a></li> --}}
